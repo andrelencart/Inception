93fa5ea6c71f3ecd238e24e29272550167b227f0
andrelencart <andre.lencart@gmail.com>
2026-02-24 18:36:36 +0000

    mariadb seems working | Starting Wordpress

--- srcs/mariadb/tools/init.sh
+++ srcs/mariadb/tools/init.sh
@@ -13,16 +13,16 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
 	while ! mariadb-admin ping --silent; do sleep 1; done
 
-	 mariadb -u root <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	mariadb -u root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
     FLUSH PRIVILEGES;
-EOSQL
+	EOSQL
 
   echo "[mariadb] Shutting down temporary server..."
-  mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+  mariadb-admin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 
   wait "$pid" || true
 fi
